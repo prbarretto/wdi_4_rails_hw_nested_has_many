581249da21801bd4ae66ae94738e5fb571783208
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-29 23:38:18 -0500

    moved new articles to user page instead of article page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+	def new
+  	@article = Article.find(params[:article_id])
+  	@user = User.find(params[:user_id])
+  	@comment = Comment.new
+	end
+
+	private
+
+	def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
