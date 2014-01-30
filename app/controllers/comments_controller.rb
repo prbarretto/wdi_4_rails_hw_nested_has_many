f9130dd9495ed0547f814bdf09935cb1055e8638
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-29 23:54:45 -0500

    can create a new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
   	@comment = Comment.new
 	end
 
+	def create
+		@user = User.find(params[:user_id])
+		@article = Article.find(params[:article_id])
+		@article.comments << comment = Comment.create!(comment_params)
+		redirect_to user_article_path(@user.id, @article.id)
+	end
+
 	private
 
 	def comment_params
